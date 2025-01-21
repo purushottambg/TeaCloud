fd53057f38d56b96b605a7d642958a202c970294
Bhratesh <bhrateshd@gmail.com>
2025-01-21 11:40:47 +0530

    removing comment

--- src/main/java/com/teaCloud/TeaClooudApplication.java
+++ src/main/java/com/teaCloud/TeaClooudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TeaClooudApplication {
-#publicClass
+
 	public static void main(String[] args) {
 		SpringApplication.run(TeaClooudApplication.class, args);
 	}
