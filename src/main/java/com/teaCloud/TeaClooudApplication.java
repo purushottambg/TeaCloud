9380691de74f88639673b8ecfb77901d9e53ecd1
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-21 11:26:04 +0530

    Project initialization has been done,all the basic files are ready

---
+++ src/main/java/com/teaCloud/TeaClooudApplication.java
@@ -0,0 +1,13 @@
+package com.teaCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaClooudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaClooudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teaCloud/TeaClooudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teaCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaClooudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
