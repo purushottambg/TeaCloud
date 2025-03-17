370feb859dab04d231cefb081da614c2b24dd43f
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-03-18 00:33:58 +0530

    Corrected rendering

--- src/main/java/com/teaCloud/controllers/LogInController.java
+++ src/main/java/com/teaCloud/controllers/LogInController.java
@@ -1,8 +1,13 @@
 package com.teaCloud.controllers;
 
 import com.teaCloud.dtos.LogInDTO;
+import com.teaCloud.entity.UserEntity;
+import com.teaCloud.repos.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +17,25 @@ import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping("/")
-
+@RequiredArgsConstructor
 public class LogInController {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
     private static final Logger logger = LoggerFactory.getLogger(LogInController.class);
 
     @PostMapping("pages/validate-login")
-    public String login(@ModelAttribute LogInDTO logInDTO){
-        logger.trace("Received details are userID {} password {}", logInDTO.getMemberID(), logInDTO.getPassword() );
-        return "pages/sign-up";
+    public String login(@ModelAttribute LogInDTO logInDTO, Model model){
+        logger.trace("Received details are userID {} password {}", logInDTO.getMemberID(), logInDTO.getPassword());
+        if (userRepository.existsById(logInDTO.getMemberID())){
+            UserEntity userEntity = userRepository.findByMemberIDAndPassword(logInDTO.getMemberID(), logInDTO.getPassword());
+            LogInDTO logInDTO1 = modelMapper.map(userEntity, LogInDTO.class);
+            model.addAttribute("logInDTO1", logInDTO1);
+            return   "pages/student-home";
+        }else {
+            model.addAttribute("Failed to authenticate", "failed");
+            return  "pages/student-home";
+        }
     }
 }
